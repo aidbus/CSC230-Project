0aa7d011a121753d0c9efd025b2110a8ae82e850
anthonysoria <143839753+anthonysoria@users.noreply.github.com>
2025-02-20 14:28:17 -0500

    Add files via upload

---
+++ Frontend/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>Learn more about our criminology research and projects.</p>
+    </div>
+  );
+}
+
+export default AboutPage;

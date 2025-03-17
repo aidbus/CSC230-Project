c3aa8436d3cceaf88623923a132c69743cfaf44d
Adam Stafford <adamstafford82@gmail.com>
2025-03-17 19:23:38 -0400

    Working pdf file upload to Database
    Continuing work on retrieval of files from mongo to the backend after upload. Also need to integrate into frontend, shouldnt be too egregious of a process. Finally, working to include pdf file names with spaces into succesful downloads/ uploads

--- backend/index.js
+++ backend/index.js
@@ -2,31 +2,34 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import authRoute from "./Routes/AuthRoute.js";
-const app = express();
+import pdfRoutes from "./Routes/PdfRoute.js";  
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 dotenv.config();
-const {MONGO_URL, PORT} = process.env;
+const { MONGO_URL, PORT } = process.env;
 
 mongoose
     .connect(MONGO_URL)
     .then(() => console.log("Successfully connected to MongoDB"))
     .catch((err) => console.log(`DB CONNECTION ERROR ${err}`));
 
-app.listen (PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-})
-
-app.use (
-    cors({
-        origin: ["http://localhost:5173"],
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        credentials: true,
-        optionsSuccessStatus: 204,
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-);
+const app = express();
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.options("*", cors());
 app.use(cookieParser());
 app.use(express.json());
+
 app.use("/", authRoute);
+//enabled pdf upload and download
+app.use("/api/pdf", pdfRoutes); 
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
